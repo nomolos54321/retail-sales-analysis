6e33db8d092326acae0f2d7ab273d61e5f83ad33
nomolos54321 <131183828+nomolos54321@users.noreply.github.com>
2026-03-06 16:48:10 +0000

    Create revenue_trend.sql

---
+++ sql/revenue_trend.sql
@@ -0,0 +1,8 @@
+SELECT
+    DATE_FORMAT(o.order_date, '%Y-%m') AS month,
+    ROUND(SUM(oi.Sales), 2) AS revenue
+FROM order_items oi
+JOIN orders o
+    ON oi.order_id = o.order_id
+GROUP BY month
+ORDER BY month;
