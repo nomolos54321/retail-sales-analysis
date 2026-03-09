261047b4fb3a3c85322b3a1c951f78e8489e0d60
nomolos54321 <131183828+nomolos54321@users.noreply.github.com>
2026-03-09 16:49:34 +0000

    Create CLV_Segment.sql

---
+++ sql/CLV_Segment.sql
@@ -0,0 +1,12 @@
+-- To show Customer Lifetime Value per segment
+
+SELECT
+    o.customer_id,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    ROUND(SUM(oi.sales),2) AS lifetime_revenue,
+    ROUND(SUM(oi.sales) / COUNT(DISTINCT o.order_id),2) AS avg_order_value
+FROM fact_order_items oi
+JOIN dim_orders o
+    ON oi.order_id = o.order_id
+GROUP BY o.customer_id
+ORDER BY lifetime_revenue DESC;
